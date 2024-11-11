cb412ffdc14c1e43e043468493af5ee161a304bc
Héctor Florido <hector.florido@gmail.com>
2024-11-11 16:20:31 +0100

    variable de session

--- src/main/java/servlets/HelloController.java
+++ src/main/java/servlets/HelloController.java
@@ -4,17 +4,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller
 public class HelloController {
 
     @RequestMapping("/hello")
-    public ModelAndView helloWorld() {
-        // Creamos el objeto ModelAndView
-        ModelAndView model = new ModelAndView("hello");
+    public void helloWorld(HttpServletResponse response) throws IOException {
+        // Creamos un mapa con los datos que queremos devolver
+        Map<String, String> responseData = new HashMap<>();
+        responseData.put("message", "¡Hola, Mundo desde Spring MVC!");
 
-        // Añadimos datos al modelo
-        model.addObject("message", "¡Hola, Mundo desde Spring MVC!");
+        // Convertimos el mapa a JSON
+        String jsonResponse = "{\"message\": \"" + responseData.get("message") + "\"}";
 
-        return model;
+        // Configuramos el tipo de contenido para la respuesta
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
+        // Enviamos el JSON como respuesta
+        response.getWriter().write(jsonResponse);
     }
 }
--- src/main/java/servlets/LoginServlet.java
+++ src/main/java/servlets/LoginServlet.java
@@ -1,5 +1,7 @@
 package servlets;
 
+import com.google.gson.Gson;
+import model.Plato;
 import service.LoginService;
 
 import javax.servlet.ServletException;
@@ -8,23 +10,70 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
     private LoginService loginService;
 
-    public LoginServlet () {
+    public LoginServlet() {
         this.loginService = new LoginService();
     }
 
-
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //TODO: forward a index.jsp
+
+        Gson gson = new Gson();
+
+        List<Plato> reservas = new ArrayList<>();
+        reservas.add(new Plato(1, "2024-11-06", "Usuario1"));
+        reservas.add(new Plato(2, "2024-11-07", "Usuario2"));
+
+        // Convertir la respuesta a JSON
+        String jsonResponse = gson.toJson(reservas);
+
+        // Configurar la respuesta
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+        resp.getWriter().write(jsonResponse);
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        this.loginService.login(req, resp, this);
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // Si los datos son enviados como JSON, hay que leer el cuerpo de la solicitud
+        String action = req.getReader().readLine();
+
+        System.out.println(action);
+
+        // Aquí puedes hacer algo con el parámetro 'action'
+        if (action != null) {
+            // Ejemplo: hacer algo dependiendo de la acción
+            System.out.println("Acción recibida: " + action);
+
+            // Responder con un mensaje
+            Map<String, String> responseData = new HashMap<>();
+            responseData.put("message", "Acción procesada: " + action);
+            Gson gson = new Gson();
+            String jsonResponse = gson.toJson(responseData);
+
+            // Configuramos la respuesta
+            resp.setContentType("application/json");
+            resp.setCharacterEncoding("UTF-8");
+            resp.getWriter().write(jsonResponse);
+        } else {
+            // Si no se recibe la acción, enviar error
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            Map<String, String> responseData = new HashMap<>();
+            responseData.put("error", "No se recibió la acción");
+
+            Gson gson = new Gson();
+            String jsonResponse = gson.toJson(responseData);
+
+            resp.getWriter().write(jsonResponse);
+        }
     }
 }
