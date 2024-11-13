5c47fd0342032ad91b8b279d3b908c87dbe4cb26
Héctor Florido <hector.florido@gmail.com>
2024-11-13 20:53:42 +0100

    update

--- src/main/java/model/Usuario.java
+++ src/main/java/model/Usuario.java
@@ -1,8 +1,12 @@
 package model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Usuario {
     private int id;
     private String nombre;
--- src/main/java/servlets/HelloController.java
+++ src/main/java/servlets/HelloController.java
@@ -1,31 +1,103 @@
 package servlets;
 
+import com.google.gson.Gson;
+import model.Usuario;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-@Controller
-public class HelloController {
+@CrossOrigin("*")
+@WebServlet("/helloworld")
+public class HelloController  extends HttpServlet {
 
-    @RequestMapping("/hello")
-    public void helloWorld(HttpServletResponse response) throws IOException {
-        // Creamos un mapa con los datos que queremos devolver
-        Map<String, String> responseData = new HashMap<>();
-        responseData.put("message", "¡Hola, Mundo desde Spring MVC!");
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 
-        // Convertimos el mapa a JSON
-        String jsonResponse = "{\"message\": \"" + responseData.get("message") + "\"}";
+        ArrayList<Usuario> usuarios = new ArrayList<>();
+        usuarios.add(new Usuario(1,"test","test", "test@test.test"));
+        usuarios.add(new Usuario(2,"test2","test2", "test2@test.test"));
+
+        Gson gsonArray = new Gson();
+
+        String jsonResponse = gsonArray.toJson(usuarios);
 
         // Configuramos el tipo de contenido para la respuesta
-        response.setContentType("application/json");
-        response.setCharacterEncoding("UTF-8");
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
 
         // Enviamos el JSON como respuesta
-        response.getWriter().write(jsonResponse);
+        resp.getWriter().write(jsonResponse);
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String action = req.getReader().readLine();
+
+        System.out.println(action);
+
+        String[] actions = action.split("#");
+        System.out.println(Arrays.toString(actions));
+
+        if (action != null) {
+
+            System.out.println("Acción recibida: " + action);
+
+            // Responder con un mensaje
+            Map<String, String> responseData = new HashMap<>();
+            responseData.put("response", "Acción procesada: " + action);
+            Gson gson = new Gson();
+            String jsonResponse = gson.toJson(responseData);
+
+            // Configuramos la respuesta
+            resp.setContentType("application/json");
+            resp.setCharacterEncoding("UTF-8");
+            resp.getWriter().write(jsonResponse);
+
+        } else {
+
+            // Si no se recibe la acción, enviar error
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            Map<String, String> responseData = new HashMap<>();
+            responseData.put("error", "ERR");
+
+            Gson gson = new Gson();
+            String jsonResponse = gson.toJson(responseData);
+
+            resp.getWriter().write(jsonResponse);
+        }
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String action = req.getReader().readLine();
+
+        System.out.println("Acción recibida: " + action);
+
+        if ("delete".equals(action)) {
+            //TODO: llamada a eliminar a la base de datos
+        }
+
+        // Responder con un mensaje
+        Map<String, String> responseData = new HashMap<>();
+        responseData.put("message", "Acción procesada: " + action);
+        Gson gson = new Gson();
+        String jsonResponse = gson.toJson(responseData);
+
+        // Configuramos la respuesta
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+        resp.getWriter().write(jsonResponse);
     }
 }
